5428128110bb2b9f381064b505c08aaaf31f25e7
Harezlak Robert <harezlak.robert7@gmail.com>
2018-11-26 21:38:40 +0100

    Add GetRecords method

--- csv_reader/Program.cs
+++ csv_reader/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +11,35 @@ namespace csv_reader
 {
   class Program
   {
+
+    static IEnumerable<T> GetRecords<T>(string filename)
+    {
+      using (var streamReader = new StreamReader(filename))
+      {
+        var configuration = new Configuration
+        {
+          Delimiter = ",",
+          HasHeaderRecord = true
+        };
+
+        using (var csvReader = new CsvReader(streamReader, configuration))
+        {
+          return csvReader.GetRecords<T>().ToArray();
+        }
+      }
+    }
+
     static void Main(string[] args)
     {
+
+      var people = GetRecords<Person>("MOCK_DATA.csv");
+
+      foreach (var person in people)
+      {
+        Console.WriteLine(person);
+      }
+
+      Console.ReadLine();
     }
   }
 }
