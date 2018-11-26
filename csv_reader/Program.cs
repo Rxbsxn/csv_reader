c0afb9b660f1bedb37905626b9f7dfc5ddd3cdf2
Harezlak Robert <harezlak.robert7@gmail.com>
2018-11-26 21:28:36 +0100

    Add CSV helper package

--- csv_reader/Program.cs
+++ csv_reader/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace csv_reader
 {
+  class Person
+  {
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+  }
   class Program
   {
     static void Main(string[] args)
