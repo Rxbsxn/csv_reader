6aa2628cdc4f90fcb41b2ec89a72fbacef15c185
Harezlak Robert <harezlak.robert7@gmail.com>
2018-11-26 21:32:27 +0100

    Create Person class and PersonMap

---
+++ csv_reader/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace csv_reader
+{
+  public class Person
+  {
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+  }
+}
---
+++ csv_reader/PersonMap.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration;
+
+namespace csv_reader
+{
+  public sealed class PersonMap : ClassMap<Person>
+  {
+    public PersonMap()
+    {
+      AutoMap();
+    }
+  }
+}
