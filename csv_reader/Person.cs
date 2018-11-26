a832a7bf150bf85e7adc0aca978eec28fb6ad965
Harezlak Robert <harezlak.robert7@gmail.com>
2018-11-26 21:41:07 +0100

    Add ToString method

--- csv_reader/Person.cs
+++ csv_reader/Person.cs
@@ -2,9 +2,14 @@
 {
   public class Person
   {
-    public int Id { get; }
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
+
+    public override string ToString()
+    {
+      return $"Id: {Id}, FirstName: {FirstName}, LastName: {LastName}, Email: {Email}";
+    }
   }
 }
